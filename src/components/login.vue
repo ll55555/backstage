fdaafe93c938771e758580d3bec6239fa9532eb5
ll55555 <577417033@qq.com>
2019-03-31 22:03:42 +0800

    第二天

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="login">
+    <!-- 居中的盒子 -->
     <div class="box">
       <!-- 标题 -->
       <h2>用户登录</h2>
-      <!-- 登录页顶部的结构 -->
+      <!-- 饿了么ui的form -->
       <el-form
         label-position="top"
         ref="loginForm"
@@ -18,8 +19,8 @@
           <el-input v-model="loginForm.password"></el-input>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="submit(loginForm)">登录</el-button>
-          <el-button type="success" @click="resetForm(loginForm)">重置</el-button>
+          <el-button type="primary" @click="submit('loginForm')">登录</el-button>
+          <el-button type="success" @click="resetForm('loginForm')">重置</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -31,52 +32,60 @@ export default {
   name: "login",
   data() {
     return {
-      //表单数据
+      // 表单数据
       loginForm: {
         username: "",
         password: ""
       },
-      // 表单验证  验证规则和表单是平级的
+      // 表单验证 规则
       rules: {
+        /*
+          required 必须
+          message 提示信息
+          trigger 触发 触发方式
+          min 最短
+          max 最长
+        * */
+
         username: [
-          { required: true, message: "用户名不能为空", trugger: "blur" },
+          { required: true, message: "用户名不能为空", trigger: "blur" },
           { min: 4, max: 16, message: "长度在4-16个字符", trigger: "blur" }
         ],
         password: [
-          { required: true, message: "密码不能为空", trugger: "blur" },
+          { required: true, message: "密码不能为空", trigger: "blur" },
           { min: 6, max: 16, message: "长度在6-16个字符", trigger: "blur" }
         ]
       }
     };
   },
-  //  方法
+  // 方法
   methods: {
     // 表单提交
     submit(formName) {
       this.$refs[formName].validate(async valid => {
         if (valid) {
-          // 成功提交
+          // 成功 提交数据
           let res = await this.$axios.post("login", this.loginForm);
           console.log(res);
-
-          if (res.data.meta.statys === 400) {
+          if (res.data.meta.status === 400) {
+            this.$message.error(res.data.meta.msg);
+          } else if (res.data.meta.status === 200) {
             this.$message.success(res.data.meta.msg);
-          } else if (this.$message.error(res.data.meatus === 200)) {
-            //  缓存数据
+            // 缓存数据
             window.sessionStorage.setItem("token", res.data.data.token);
-            //   跳转到主页
+            // 跳转 到主页
             this.$router.push("/");
           }
         } else {
           // 失败
-          this.$message.error("数据格式错误,请根据提示修改");
+          this.$message.error("数据格式错误，请根据提示修改");
           return false;
         }
       });
     },
-    // 重置表单   获取饿了么表单 调用 重置方法
+    // 重置表单
     resetForm(formName) {
-      this.$refs[formName].resetFields;
+      this.$refs[formName].resetFields();
     }
   }
 };
@@ -85,7 +94,6 @@ export default {
 <style lang='scss'>
 .login {
   height: 100%;
-  //   利用弹性布局
   display: flex;
   justify-content: center;
   align-items: center;
@@ -94,7 +102,7 @@ export default {
     width: 550px;
     padding: 35px;
     border-radius: 10px;
-    // 怪异盒模型 就是盒子的内敛
+    // 怪异盒模型
     box-sizing: border-box;
     height: 420px;
     background: white;
@@ -104,4 +112,3 @@ export default {
   }
 }
 </style>
-
--- src/main.js
+++ src/main.js
@@ -1,19 +1,54 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-// 导入axios
-import axios from 'axios'
-// 设置基地址
-axios.defaults.baseURL="http://localhost:8888/api/private/v1/"
-Vue.prototype.$axios= axios
-
 // 导入全局样式
 import './assets/base.scss'
 
 // 导入饿了么ui
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-Vue.use(ElementUI);
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
+
+// 导入axios
+import axios from 'axios'
+// 设置基地址
+axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/'
+// 设置axios拦截器
+// 请求拦截器 请求之前统一设置一些内容 比如 token
+axios.interceptors.request.use(
+  function(config) {
+    // Do something before request is sent
+    // 统一设置token
+    config.headers.Authorization = window.sessionStorage.getItem('token')
+    return config
+  },
+  function(error) {
+    // Do something with request error
+    return Promise.reject(error)
+  }
+)
+
+// 响应拦截器 统一处理响应
+axios.interceptors.response.use(
+  function(response) {
+    // Do something with response data
+    console.log('响应拦截')
+    console.log(response)
+    // 统一进行弹框
+    if ([200, 201, 204].indexOf(response.data.meta.status) != -1) {
+      Vue.prototype.$message.success(response.data.meta.msg)
+    } else {
+      Vue.prototype.$message.warning(response.data.meta.msg)
+    }
+    return response
+  },
+  function(error) {
+    // Do something with response error
+    return Promise.reject(error)
+  }
+)
+
+Vue.prototype.$axios = axios
 
 Vue.config.productionTip = false
 
@@ -25,4 +60,3 @@ new Vue({
   // 挂载到Vue实例上
   router
 }).$mount('#app')
-
